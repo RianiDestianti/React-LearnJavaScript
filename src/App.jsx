2a1b0cb7a4843f90bf9dc84622aa6ac985d0a03a
RianiDestianti <rianidestiantii@gmail.com>
2025-05-31 19:22:34 +0700

    refactor: apply props.text to Button

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const Button = (props) => {
       className={`h-10 px-6 font-semibold rounded-md ${props.variant} text-white`}
       type="submit"
     >
-      Buy now
+      {props.text}
     </button>
   );
 };
@@ -15,8 +15,8 @@ function App() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-blue-600">
       <div className="flex gap-x-3">
-        <Button variant="bg-red-700" />
-        <Button variant="bg-slate-700" />
+        <Button variant="bg-red-700" text="Login" />
+        <Button variant="bg-slate-700" text="LogOut"/>
         <Button variant="bg-black" />
       </div>
     </div>
