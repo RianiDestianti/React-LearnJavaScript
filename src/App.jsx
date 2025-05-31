0eabb827a892764e8d97479a039b73e6af9ecb08
RianiDestianti <rianidestiantii@gmail.com>
2025-05-31 18:34:14 +0700

    feat: create Button component (class)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-function App(){
-  return(
+import React from "react";
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button
+        className="h-10 px-6 font-semibold rounded-md bg-black text-white"
+        type="submit"
+      >
+        Buy now
+      </button>
+    );
+  }
+}
+
+
+function App() {
+  return (
     <div className="flex justify-center bg-blue-600 min-h-screen items-center">
-      <div>Hello World</div>
+      <div>
+        <Button />
+        <Button />
+        <Button />
+      </div>
     </div>
   );
 }
